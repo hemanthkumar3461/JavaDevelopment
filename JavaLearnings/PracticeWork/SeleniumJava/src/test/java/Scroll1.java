dc9e949ae8e63f89db08eaf8e1bb3549b678acf3
hemanth <hemanthkumar3461@gmail.com>
2022-03-06 19:34:33 +0530

    Added files

--- JavaLearnings/PracticeWork/SeleniumJava/src/test/java/Scroll1.java
+++ JavaLearnings/PracticeWork/SeleniumJava/src/test/java/Scroll1.java
@@ -1,20 +1,8 @@
-
-public class Scroll1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-
-
-
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-driver = webdriver.Chrome()
-driver.get('http://www.w3schools.com/')
-target = driver.find_element_by_link_text('BROWSE TEMPLATES')
-actions = ActionChains(driver)
-actions.move_to_element(target)
-actions.perform()
+/*
+ * 
+ * from selenium import webdriver from selenium.webdriver.common.action_chains
+ * import ActionChains driver = webdriver.Chrome()
+ * driver.get('http://www.w3schools.com/') target =
+ * driver.find_element_by_link_text('BROWSE TEMPLATES') actions =
+ * ActionChains(driver) actions.move_to_element(target) actions.perform()
+ */
