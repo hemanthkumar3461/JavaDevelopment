2454e4c6948805ec2c506181d6b9dd9b560a560f
hemanth <hemanthkumar3461@gmail.com>
2022-03-07 17:09:36 +0530

    Added codes

--- JavaLearnings/PracticeWork/SeleniumJava/src/test/java/Scroll1.java
+++ JavaLearnings/PracticeWork/SeleniumJava/src/test/java/Scroll1.java
@@ -4,5 +4,6 @@
  * import ActionChains driver = webdriver.Chrome()
  * driver.get('http://www.w3schools.com/') target =
  * driver.find_element_by_link_text('BROWSE TEMPLATES') actions =
+ * 
  * ActionChains(driver) actions.move_to_element(target) actions.perform()
  */
