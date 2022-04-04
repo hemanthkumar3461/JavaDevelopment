310f7af29ed503e9dbe97c6e8773571280ebade2
hemanth <hemanthkumar3461@gmail.com>
2022-04-04 23:41:15 +0530

    Added codes

--- JavaLearnings/PracticeWork/SeleniumPractice/src/test/java/SeleniumTest1/GoogleSearch.java
+++ JavaLearnings/PracticeWork/SeleniumPractice/src/test/java/SeleniumTest1/GoogleSearch.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class GoogleSearch {
 	public static void main(String[] args) {
+		
 		// Create a new instance of the html unit driver
 		// Notice that the remainder of the code relies on the interface,
 		// not the implementation.
